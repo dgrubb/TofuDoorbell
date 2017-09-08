77f99416fdef3b9f407df3812dfa4134b70d3a13
dgrubb <davidanthonygrubb@gmail.com>
2017-09-08 09:55:12 -0500

    Add static application skeleton.

---
+++ webservice/static/js/main.js
@@ -0,0 +1,13 @@
+/* 
+ * File: main.js
+ * Author: dgrubb
+ * Date: 09/08/2017
+ *
+ * Uses the Jquery .ready() callback to provide a starting point
+ * for the rest of the application.
+ */
+
+$(document).ready(function() {
+    // Start here
+});
+
