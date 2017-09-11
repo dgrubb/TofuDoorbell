fe5ff1138dfe5fe0938b7a8c7b6e0d9edf026182
dgrubb <davidanthonygrubb@gmail.com>
2017-09-11 16:43:02 -0500

    Add a module for handling the local audio content list.

---
+++ webservice/static/js/content.js
@@ -0,0 +1,23 @@
+/*
+ * File: content.js
+ * Author: dgrubb
+ * Date: 09/11/2017
+ *
+ * Functionality for fetching and parsing the local content.
+ */
+
+var API = (function() {
+    'use strict';
+
+    function fetchContentListCallback() {
+    }
+
+    function populateContent() {
+    }
+
+    var api = {
+    };
+
+    return api;
+}());
+
